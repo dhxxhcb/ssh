12f5227924c5b8113671c0395f754cc0ca0ca281
hcb <784875590@qq.com>
2017-05-21 21:14:56 +0800

    thinkpad HelloGit

--- ssh_job/first_pro/src/com/hcb/gittest/HelloGit.java
+++ ssh_job/first_pro/src/com/hcb/gittest/HelloGit.java
@@ -6,6 +6,6 @@ package com.hcb.gittest;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Hello git thinkpad");
     }
 }
