2be5ca6a7065d4dacd7a614b4ec05c6b10dd96c2
hcb <784875590@qq.com>
2017-05-22 14:58:35 +0800

    dell 测试提交，

--- ssh_job/first_pro/src/com/hcb/testgit/HelloGit.java
+++ ssh_job/first_pro/src/com/hcb/testgit/HelloGit.java
@@ -8,6 +8,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git by dell....");
 	    System.out.println("hello git by dell...dell for conf");
+
+        System.out.println("dell 提交");
     }
 }
 
